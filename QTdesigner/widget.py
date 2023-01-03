b1b81df035c5b5d5331fc129098b4ee836e6d808
qinjunlong <47288391+qinjunlong@users.noreply.github.com>
2023-01-04 02:29:23 +0900

    Add files via upload

--- QTdesigner/widget.py
+++ QTdesigner/widget.py
@@ -7,9 +7,10 @@ import importlib
 
 from MyPlug.TestFunction import Classtest_02
 from MyPlug.Function_01 import ExportCamer_C
+from MyPlug.Function_02 import RotateClear
 importlib.reload(Classtest_02)
 importlib.reload(ExportCamer_C)
-
+importlib.reload(RotateClear)
 CURRENT_FILE = os.path.normpath(__file__)
 path, ext = os.path.splitext(CURRENT_FILE)
 UI_FILE = path + ".ui"
@@ -25,6 +26,7 @@ class TRSConnectorWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         self.widget.Pbutton_A.clicked.connect(self.click_A)
         self.widget.Pbutton_B.clicked.connect(self.click_B)
         self.widget.Pbutton_C.clicked.connect(self.click_C)
+        self.widget.Fun_02_Button.clicked.connect(self.click_Fun2)
         
 
 
@@ -41,3 +43,12 @@ class TRSConnectorWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
         Btime = self.widget.Time.value()
         clt_C = ExportCamer_C.ExportCamer_C(Btime)
         clt_C.CallBTN()
+
+    def click_Fun2(self):
+        Check_RotX = self.widget.Fun_02_rotX.isChecked()
+        Check_RotY = self.widget.Fun_02_rotY.isChecked()
+        Check_RotZ = self.widget.Fun_02_rotZ.isChecked()
+        
+        Clt_Fun2 = RotateClear.RotateClear(Check_RotX,Check_RotY,Check_RotZ)
+        Clt_Fun2.Clear()
+
